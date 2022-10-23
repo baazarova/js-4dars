dd562ff43e3ca94efca8d718c3152c1b4cecc98e
baazarova <iskandarovaogiloy@gmail.com>
2022-10-23 08:42:50 +0500

    3ga bolish masalasi

--- index.js
+++ index.js
@@ -1,30 +1,41 @@
-let even = Number(0);
-let odd = Number(0);
+//1000 gacha bolgan sonlarning juft sonlar yigindisi va toq sinlar yigindisi ning yigindisini topish--
+
+// let even = Number(0);
+// let odd = Number(0);
+
+// function findNumber(num){
+//     for(i = 0 ; i <=num ; i++ ){
+//         if(i % 2 == 0){
+//             even +=i
+         
+//         }
+//         else{
+//             odd += i
+         
+//         }
+//     }
+ 
+// }
+
+// findNumber(1000);
+
+// let result = Number(even + odd)
+
+// console.log(result);
+
+
+
+//1000 gacha bolgan sonlarning ichida 3 ga bolinadigan sonlar yig'indisini topish:
+
+let son = Number(0);
 
 function findNumber(num){
-    for(i = 0 ; i <=num ; i++ ){
-        if(i % 2 == 0){
-            even +=i
-         
-        }
-        else{
-            odd += i
-         
+    for(i = 0; i < num; i++){
+        if(i % 3 == 0){
+            son = i + son;
         }
     }
- 
 }
-
 findNumber(1000);
 
-// if (even > odd) {
-//     let result = Math.round(even / odd);
-//     console.log(result);
-//   } else {
-//     let result = Math.round(odd / even);
-//     console.log(result);
-//   }
-
-let result = Number(even + odd)
-
-console.log(result);
+console.log(son);
